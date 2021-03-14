69f9a777d03189d5a3c83ad483169618b1c75d0d
Replit user <replituser@example.com>
2021-03-14 00:05:48 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -83,6 +83,9 @@ typedef struct {
 Vector * crearVector(int n) {
   Vector *creacion = (Vector *) calloc (n, sizeof(Vector));
   creacion->capacidad = n;
+  for (int i = 0 ; i < n ; i++){
+    if (creacion->datos[i] != 0) creacion->datos = 0;
+  }
   return creacion;
 }
 
